605bd58d641443cef668c57e2c80c14286949b33
DiegoHrz <hernandezwdiego@gmail.com>
2023-10-10 23:45:58 -0500

    Add initialization

--- 03-sequelize/demo-class/index.js
+++ 03-sequelize/demo-class/index.js
@@ -2,6 +2,7 @@ const server = require("./app");
 const sequelize = require(".db");
 const PORT = 3001
 //PORT 3001
+
 server.listen(PORT, async () => {
   await sequelize.sinc();
   console.log(`server listening on ${PORT}`);
