31c0d81ce124dd8cf6b7da432e757d712234506e
DiegoHrz <hernandezwdiego@gmail.com>
2023-11-03 01:10:19 -0500

    Add Homework Sequelize I

--- 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Ability.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Ability.js
@@ -1,5 +1,25 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
-module.exports = sequelize => {
-  sequelize.define('Ability', {});
+module.exports = (sequelize) => {
+  sequelize.define("Ability", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //nos piden la combinación "name" + "mana_cost" debe ser única y llamarse 'compositeIndex'.
+      unique: "compositeIndex",
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    mana_cost: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      //nos piden la combinación "name" + "mana_cost" debe ser única y llamarse 'compositeIndex'.
+      unique: "compositeIndex",
+      validate: {
+        min: 10.0,
+        max: 250.0,
+      },
+    },
+  });
 };
--- 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Character.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Character.js
@@ -1,5 +1,44 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-   sequelize.define('Character', {});
+  sequelize.define(
+    "Character",
+    {
+      code: {                         // 5 caracteres max
+        type: DataTypes.STRING(5),     // 5 caracteres max
+        primaryKey: true,             // Validación: el valor no puede ser HENRY
+        validate: {
+          isNotHenry(value) {
+            if (value.toLowerCase() === "henry") throw new Error("is henry");
+          },
+        },
+      },                               // Mistake 1: Removed the extra comma
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,                  // Debe ser único
+        validate: {
+          notIn: [["Henry", "SoyHenry", "Soy Henry"]],  // Operador notIn que permite introducir valores que no queremos en nuestra data
+        },
+      },
+      age: { type: DataTypes.INTEGER },
+      race: {
+        type: DataTypes.ENUM(
+          "Human",
+          "Elf",
+          "Machine",
+          "Demon",
+          "Animal",
+          "Other"
+        ),
+        defaultValue: "other",         // Tendrá un valor por defecto considerado "other"
+      },
+      hp: { type: DataTypes.FLOAT, allowNull: false },
+      mana: { type: DataTypes.FLOAT, allowNull: false },
+    },
+    {
+      timestamps: false,
+    }
+  );
 };
